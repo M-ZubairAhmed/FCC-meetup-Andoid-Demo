dd16280e608b8ad58a0b399c59f2ac82ce8b45af
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-03-04 02:06:23 +0530

    Step 8 : Delete button added

--- app/src/main/java/com/fcchyd/androidonefcc/CustomArrayAdapter.java
+++ app/src/main/java/com/fcchyd/androidonefcc/CustomArrayAdapter.java
@@ -7,6 +7,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -34,6 +36,15 @@ public class CustomArrayAdapter extends ArrayAdapter<TaskData> {
 
         TextView taskTextView = (TextView) view.findViewById(R.id.task_text_field);
         taskTextView.setText(taskData.getTask());
+
+        ImageView deleteButton = (ImageView) view.findViewById(R.id.task_delete_button);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                arrayList.remove(taskData);
+                notifyDataSetChanged();
+            }
+        });
         return view;
     }
 }
