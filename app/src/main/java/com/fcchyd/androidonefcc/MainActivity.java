055bc49d1258fec81aadb9bfa3038706a90e70b7
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-03-03 18:21:25 +0530

    Step 2 : Color theme for the app

--- app/src/main/java/com/fcchyd/androidonefcc/MainActivity.java
+++ app/src/main/java/com/fcchyd/androidonefcc/MainActivity.java
@@ -2,12 +2,9 @@ package com.fcchyd.androidonefcc;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,44 +14,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // 1. Putting up title in action bar
-        titleInActionBar();
+        titleInActionBar("freeCodeCamp-Android");
 
+        //2.  Floatation action button initializing and setting up listener below
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.floating_action_button_xml);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Write Fab method here
             }
         });
     }
-/*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-*/
-    protected void titleInActionBar(){
+    /**
+     * This method puts up the title in toolbar or action bar of app
+     *
+     * @param title : The input title
+     *              to know more about java documentation syntax
+     *              http://www.oracle.com/technetwork/articles/java/index-137868.html
+     */
+    protected void titleInActionBar(String title) {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        this.setTitle("freeCodeCamp-Android");
+        this.setTitle(title);
 
     }
 }
