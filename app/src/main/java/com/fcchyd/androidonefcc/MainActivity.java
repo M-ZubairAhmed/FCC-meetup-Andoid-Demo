0dbe7e5a3dacba38df7cd4291bbfa4a8cabce375
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-03-03 23:42:27 +0530

    Step 6 : Create basic array adapter

--- app/src/main/java/com/fcchyd/androidonefcc/MainActivity.java
+++ app/src/main/java/com/fcchyd/androidonefcc/MainActivity.java
@@ -6,15 +6,25 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     protected String inputTaskString;
     protected EditText inputTaskEditText;
+    //Initializing Array List
+    protected ArrayList<String> arrayList;
+    protected ListView listView;
+    protected ArrayAdapter<String> arrayAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,11 +36,17 @@ public class MainActivity extends AppCompatActivity {
         // Floatation action button initializing and setting up listener below
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.floating_action_button_xml);
         fab.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
                 createNewTaskAlertDialog();
             }
         });
+
+        arrayList = new ArrayList<>();
+        arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,arrayList);
+        listView = (ListView)findViewById(R.id.content_main_list_view);
+        listView.setAdapter(arrayAdapter);
     }
 
     /**
@@ -57,18 +73,28 @@ public class MainActivity extends AppCompatActivity {
                 .setPositiveButton("Add", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        getInputTask(alertDialogView);
+                        inputTaskString = getInputTask(alertDialogView);
+                        arrayList.add(inputTaskString);
+                        for (String element : arrayList){
+                            Log.v("AppHasInThisArrayList",element);
+                        }
                     }
                 })
                 .create();
         alertDialog.show();
     }
 
-    protected void getInputTask(View alertDialogView){
+    /**
+     * This method takes input from edit text field and stores in form of string
+     * @param alertDialogView   The inflated custom view for alert dialog
+     * @return String variable of entered text in field
+     */
+    protected String getInputTask(View alertDialogView){
         inputTaskEditText = (EditText) alertDialogView.findViewById(R.id.task_input_field_xml);
-        inputTaskString = inputTaskEditText.getText().toString();
-        if (inputTaskString.length() != 0){
+        String str = inputTaskEditText.getText().toString();
+        if (str.length() != 0){
             Toast.makeText(getApplicationContext(), inputTaskString, Toast.LENGTH_SHORT).show();
         }
+        return str.trim();
     }
 }
