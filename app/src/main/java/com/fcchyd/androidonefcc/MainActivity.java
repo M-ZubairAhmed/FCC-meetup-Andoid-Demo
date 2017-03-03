769cd7b2244f1fd403aef62b2de2df0b0baf715e
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-03-03 22:41:28 +0530

    Step 4 - Make an alert dialog for input of task

--- app/src/main/java/com/fcchyd/androidonefcc/MainActivity.java
+++ app/src/main/java/com/fcchyd/androidonefcc/MainActivity.java
@@ -1,9 +1,12 @@
 package com.fcchyd.androidonefcc;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,15 +16,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 1. Putting up title in action bar
+        // Putting up title in action bar
         titleInActionBar("freeCodeCamp-Android");
 
-        //2.  Floatation action button initializing and setting up listener below
+        // Floatation action button initializing and setting up listener below
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.floating_action_button_xml);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Write Fab method here
+                createNewTask();
             }
         });
     }
@@ -38,5 +41,21 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         this.setTitle(title);
 
+    }
+
+    protected void createNewTask(){
+        LayoutInflater layoutInflater = LayoutInflater.from(MainActivity.this);
+        View alertDialogView = layoutInflater.inflate(R.layout.alert_dialog,null);
+        AlertDialog alertDialog = new AlertDialog.Builder(this)
+                .setView(alertDialogView)
+                .setTitle(R.string.alert_dialog_title)
+                .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //
+                    }
+                })
+                .create();
+        alertDialog.show();
     }
 }
